aa4589e7f2f460206ac813d90fff03a0684f9ef4
Djamel Grine <djamel.grine@gmail.com>
2017-10-27 14:40:33 +0200

    Clean-up.

--- base/finance/mortgage/loan.py
+++ base/finance/mortgage/loan.py
@@ -25,18 +25,31 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
 # POSSIBILITY OF SUCH DAMAGE.
 ################################################################################
+from base.utilities.texttable import Texttable
 import numpy as np
 import collections
 
-Installment = collections.namedtuple('Installment', 'idx interest capital payment debt')
+Installment = collections.namedtuple('Installment', 'idx interest capital payment total_interest total_capital debt')
 
 class Loan(object):
+
     def __init__(self, principal, annual_interest_rate, term_in_years):
         super(Loan, self).__init__()
         self.principal = principal
         self.annual_interest_rate = annual_interest_rate
         self.term = term_in_years
-        self.nr_months = self.term * 12
+
+    @property
+    def nr_months(self):
+        return self.term * 12
+
+    @property
+    def total_cost(self):
+        return self.principal + self.total_interest
+
+    @property
+    def total_interest(self):
+        return self.yearly_installments[-1].total_interest
 
     @property
     def monthly_interest_rate(self):
@@ -48,39 +61,65 @@ class Loan(object):
 
     @property
     def monthly_installments(self):
-        data = [Installment(idx = 0, interest = 0, capital = 0, payment = 0, debt = self.principal)]
+        if not hasattr(self, '_monthly_installments'):
+            self._monthly_installments = self._compute_monthly_installments()
+        return self._monthly_installments
 
+    @property
+    def yearly_installments(self):
+        if not hasattr(self, '_yearly_installments'):
+            self._yearly_installments = self._compute_yearly_installments()
+        return self._yearly_installments
+
+    def summary(self, term = 'month'):
+        table = Texttable()
+        rows = [[term.capitalize(), 'Interest', 'Capital', 'Payment', 'Total Interest', 'Total Capital', 'Debt']]
+        installments = getattr(self, '{}ly_installments'.format(term))
+        rows.extend([[dp.idx, dp.interest, dp.capital, dp.payment, dp.total_interest, dp.total_capital, dp.debt] for dp in installments])
+        table.add_rows(rows)
+        return table.draw()
+
+    def _compute_monthly_installments(self):
+        data = [Installment(idx = 0, interest = 0, capital = 0, payment = 0, total_interest = 0, total_capital = 0, debt = self.principal)]
         for month in range(1, self.nr_months + 1):
             capital = (1.+self.monthly_interest_rate)**(month - 1)*(self.monthly_payment - self.monthly_interest_rate*self.principal)
             interest = self.monthly_payment - capital
             payment = capital + interest
+            total_interest = data[-1].total_interest + interest
+            total_capital = data[-1].total_capital + capital
             debt = data[-1].debt - capital
-            installment = Installment(idx = month, interest = interest, capital = capital, payment = payment, debt = debt)
+            installment = Installment(
+                    idx = month, 
+                    interest = interest,
+                    capital = capital,
+                    payment = payment, 
+                    total_interest = total_interest, 
+                    total_capital = total_capital, 
+                    debt = debt)
             data.append(installment)
         return data
 
-    @property
-    def yearly_installments(self):
+    def _compute_yearly_installments(self):
         def chunks(l, n):
             for i in range(0, len(l), n): yield l[i:i + n]
-
         data = [self.monthly_installments[0]]
-
         for year, monthly_installments in enumerate(chunks(self.monthly_installments[1:], 12)):
             capital = np.sum([installment.capital for installment in monthly_installments])
             # Alternative way, but less precise if I'm not mistaken
             # interest = self.annual_interest_rate * data[-1].debt
             interest = np.sum([installment.interest for installment in monthly_installments])
             payment = capital + interest
+            total_interest = data[-1].total_interest + interest
+            total_capital = data[-1].total_capital + capital
             debt = monthly_installments[-1].debt
-            installment = Installment(idx = year + 1, interest = interest, capital = capital, payment = payment, debt = debt)
+            installment = Installment(
+                    idx = year + 1, 
+                    interest = interest, 
+                    capital = capital, 
+                    total_interest = total_interest, 
+                    total_capital = total_capital, 
+                    payment = payment, 
+                    debt = debt)
             data.append(installment)
         return data
-
-    def summary(self, term = 'month'):
-        feed_name = '{}ly_installments'.format(term)
-        feed = getattr(self, feed_name) 
-        for n, installment in enumerate(feed):
-            line = "{}: {}, Interest: {}, Capital: {}, Payment: {}, Debt: {}".format(term, installment.idx, installment.interest, installment.capital, installment.payment, installment.debt)
-            print(line)
 
